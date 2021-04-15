9b817dc61f9d6832fc7f96e31784b352eeaab1b7
Scoutacus <vanguardacus@gmail.com>
2021-04-15 12:47:20 -0400

    corrected filetype of functions.js, added an additional function to test if a variable is an array

---
+++ functions.js
@@ -0,0 +1,15 @@
+function squared(num){
+    return num * num;
+}
+
+function cubed(num){
+    return num * num * num;
+}
+
+function circumference(num){
+    return (num * 2) * 3.14;
+}
+
+function isArray(x) {
+    return x.constructor.toString().indexOf("Array") > -1;
+} 
